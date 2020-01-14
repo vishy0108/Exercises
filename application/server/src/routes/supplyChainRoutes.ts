dd3b90edcca59583b4f04137fc318244bb380761
viswanath kapavarapu <viswanath.iit@gmail.com>
2020-01-15 02:39:43 +0530

    updated supply chain routes

--- application/server/src/routes/supplyChainRoutes.ts
+++ application/server/src/routes/supplyChainRoutes.ts
@@ -47,27 +47,134 @@ export const supplyChainRouter = (app: Application) => {
     }
   });
 
-  router.put("/orders/:id/receive", (req: Request, res: Response) => {
-    //
+  router.put("/orders/:id/receive", async (req: Request, res: Response) => {
+    try {
+      const orderID = req.params.id;
+      const receiveOrderResponse = await fabricCAClient.submitTransaction(
+        CONTRACT_METHODS.RECEIVE_ORDER,
+        orderID
+      );
+      return res.status(STATUS_SUCCESS).send(receiveOrderResponse);
+    } catch (error) {
+      return handleError(
+        res,
+        error,
+        `There was a problem in receiving order with ID ${request.params.id}`
+      );
+    }
   });
 
-  router.put("/orders/:id/assign-shipper", (req: Request, res: Response) => {
-    //
+  router.put(
+    "/orders/:id/assign-shipper",
+    async (req: Request, res: Response) => {
+      try {
+        const orderID = req.params.id;
+        const assignShipperResponse = await fabricCAClient.submitTransaction(
+          CONTRACT_METHODS.ASSIGN_SHIPPER,
+          orderID
+        );
+        return res.status(STATUS_SUCCESS).send(assignShipperResponse);
+      } catch (error) {
+        return handleError(
+          res,
+          error,
+          `There was a problem assigning shipper for order, ${request.params.id}`
+        );
+      }
+    }
+  );
+
+  router.put(
+    "/orders/:id/create-shipment",
+    async (req: Request, res: Response) => {
+      try {
+        const orderID = req.params.id;
+        const assignShipperResponse = await fabricCAClient.submitTransaction(
+          CONTRACT_METHODS.CREATE_SHIPMENT,
+          orderID
+        );
+        return res.status(STATUS_SUCCESS).send(assignShipperResponse);
+      } catch (error) {
+        return handleError(
+          res,
+          error,
+          `There was a problem in creating shipment for order, ${request.params.id}`
+        );
+      }
+    }
+  );
+
+  router.put(
+    "/orders/:id/transit-shipment",
+    async (req: Request, res: Response) => {
+      try {
+        const orderID = req.params.id;
+        const transportShipmentResponse = await fabricCAClient.submitTransaction(
+          CONTRACT_METHODS.TRANSPORT_SHIPEMNT,
+          orderID
+        );
+        return res.status(STATUS_SUCCESS).send(transportShipmentResponse);
+      } catch (error) {
+        return handleError(
+          res,
+          error,
+          `There was a problem initiating shipment transport for order, ${request.params.id}`
+        );
+      }
+    }
+  );
+
+  router.put(
+    "/orders/:id/receive-shipment",
+    async (req: Request, res: Response) => {
+      try {
+        const orderID = req.params.id;
+        const receiveShipmentResponse = await fabricCAClient.submitTransaction(
+          CONTRACT_METHODS.RECEIVE_SHIPMENT,
+          orderID
+        );
+        return res.status(STATUS_SUCCESS).send(receiveShipmentResponse);
+      } catch (error) {
+        return handleError(
+          res,
+          error,
+          `There was a problem in receiving shipment for order, ${request.params.id}`
+        );
+      }
+    }
+  );
+
+  router.delete("/orders/:id", async (req: Request, res: Response) => {
+    try {
+      const orderID = req.params.id;
+      const deleteOrderResponse = await fabricCAClient.submitTransaction(
+        CONTRACT_METHODS.DELETE_ORDER,
+        orderID
+      );
+      return res.status(STATUS_SUCCESS).send(deleteOrderResponse);
+    } catch (error) {
+      return handleError(
+        res,
+        error,
+        `There was a problem in deleting order, ${request.params.id}`
+      );
+    }
   });
 
-  router.put("/orders/:id/create-shipment", (req: Request, res: Response) => {
-    //
-  });
-
-  router.put("/orders/:id/transit-shipment", (req: Request, res: Response) => {
-    //
-  });
-
-  router.put("/orders/:id/receive-shipment", (req: Request, res: Response) => {
-    //
-  });
-
-  router.delete("/orders/:id", (req: Request, res: Response) => {
-    //
+  router.get("/order-history/:id", async (req: Request, res: Response) => {
+    try {
+      const orderID = req.params.id;
+      const orderHistoryResponse = await fabricCAClient.submitTransaction(
+        CONTRACT_METHODS.QUERY_ORDER_HISTORY,
+        orderID
+      );
+      return res.status(STATUS_SUCCESS).send(orderHistoryResponse);
+    } catch (error) {
+      return handleError(
+        res,
+        error,
+        `There was a problem fetching history for order, ${request.params.id}`
+      );
+    }
   });
 };
